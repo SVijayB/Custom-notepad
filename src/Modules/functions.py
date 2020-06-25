579706c732d5dcce97f50c9151318e227b5af61d
vijay <vijaybalajitheboss@gmail.com>
2020-06-25 14:22:02 +0530

    Added in all the menu options

--- src/Modules/functions.py
+++ src/Modules/functions.py
@@ -9,3 +9,51 @@ def Save_as():
 
 def Close():
     pass
+
+def Cut():
+    pass
+
+def Copy():
+    pass
+
+def Paste():
+    pass
+
+def Erase():
+    pass
+
+def Clear_screen():
+    pass
+
+def Date():
+    pass
+
+def Time():
+    pass
+
+def Date_and_time():
+    pass
+
+def Text_Colour():
+    pass
+
+def No_Format():
+    pass
+
+def Bold():
+    pass
+
+def Italic():
+    pass
+
+def Underline():
+    pass
+
+def Background():
+    pass
+
+def View_help():
+    pass
+
+def Feedback():
+    pass
