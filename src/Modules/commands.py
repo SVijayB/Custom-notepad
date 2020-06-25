efb39e7fe09661cae4788a6c9b0e486e5f272b26
vijay <vijaybalajitheboss@gmail.com>
2020-06-25 15:26:10 +0530

    Added in the new and open file functionality

--- src/Modules/commands.py
+++ src/Modules/commands.py
@@ -1,8 +1,23 @@
+from tkinter import *
+from tkinter import filedialog
+from tkinter.messagebox import askyesno
+from Notepad import *
+
 def New_file():
-    pass
+    if askyesno("Custom-Notepad", "Do you want to save changes?"):
+        filename = filedialog.asksaveasfilename()
+        if filename:
+            data = content.get(1.0, END)
+            open(filename, 'w').write(data)
+    else:
+        content.delete(1.0, END)
 
 def Open_file():
-    pass
+    if askyesno("Custom-Notepad", "Do you want to save changes?"):
+        filename = filedialog.asksaveasfilename()
+        if filename:
+            data = content.get(1.0, END)
+            open(filename, 'w').write(data)
 
 def Save_as():
     pass
